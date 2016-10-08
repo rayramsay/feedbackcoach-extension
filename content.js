36cfa244e44c82a4ef61a090c6b67400a4fdcae4
Ray Ramsay <rachel.ramsay@outlook.com>
2016-10-08 15:56:51 -0700

    Adds transitions.

--- content.js
+++ content.js
@@ -2,12 +2,13 @@ chrome.runtime.onMessage.addListener(function(msg, sender){
     if(msg == "toggle"){
         toggle();
     }
-    if(msg == "showToast"){
+    if(msg == "toggleToast"){
         toggleToast();
     }
 })
 
 var iframe = document.createElement('iframe');
+iframe.style.background = "gray";
 iframe.style.height = "100%";
 iframe.style.width = "0px";
 iframe.style.position = "fixed";
@@ -15,18 +16,19 @@ iframe.style.top = "0px";
 iframe.style.right = "0px";
 iframe.style.zIndex = "9000000000000000000";
 iframe.frameBorder = "none";
+iframe.style.transition = "width 2s";
 iframe.src = chrome.extension.getURL("panel.html");
 document.body.appendChild(iframe);
 
 var toast = document.createElement('div'); 
 toast.style.background = "gray";
 toast.style.height = "100px";
-toast.style.width = "100px";
+toast.style.width = "0px";
 toast.style.position = "fixed";
 toast.style.bottom = "0px";
 toast.style.right = "0px";
 toast.style.zIndex = "9000000000000000000";
-toast.frameBorder = "none"; 
+toast.style.transition = "width 2s";
 toast.src = chrome.extension.getURL("toast.html");
 document.body.appendChild(toast);
 
@@ -40,5 +42,10 @@ function toggle(){
 }
 
 function toggleToast(){
-    toast.style.width="100px";
+    if(iframe.style.width == "0px"){
+        iframe.style.width="200px";
+    }
+    else{
+        iframe.style.width="0px";
+    }
 }

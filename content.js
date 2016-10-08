2e3a910796265a5732d3a1be7e5d53dc7f0dc392
Zeus <lalkaka.zeus@gmail.com>
2016-10-08 15:11:16 -0700

    Fix iframe scrolling

--- content.js
+++ content.js
@@ -4,15 +4,14 @@ chrome.runtime.onMessage.addListener(function(msg, sender){
     }
 })
 
-var iframe = document.createElement('iframe'); 
-iframe.style.background = "blue";
+var iframe = document.createElement('iframe');
 iframe.style.height = "100%";
 iframe.style.width = "0px";
 iframe.style.position = "fixed";
 iframe.style.top = "0px";
 iframe.style.right = "0px";
 iframe.style.zIndex = "9000000000000000000";
-iframe.frameBorder = "none"; 
+iframe.frameBorder = "none";
 iframe.src = chrome.extension.getURL("panel.html");
 
 document.body.appendChild(iframe);
